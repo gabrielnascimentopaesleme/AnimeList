67b41b1cdfabac528f7297734ef5325b7e8a65d0
Gabriel Paes Leme <gabrielnascimentopaesleme@gmail.com>
2024-12-10 17:21:16 -0300

    Criação do componente de exibição de animes

---
+++ src/app/components/AnimeList.tsx
@@ -0,0 +1,76 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+type Anime = {
+  id: string;
+  name: string;
+  releaseYear: number;
+  episodesCount: number;
+  status: string;
+  summary: string;
+  imageUrl: string;
+  streamingPlatforms: string[];
+};
+
+const AnimeList = () => {
+  const [animes, setAnimes] = useState<Anime[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchedAnimes = async () => {
+      try {
+        const response = await fetch('/api/animes');
+        if (!response.ok) {
+          throw new Error(`Falha na requisição: ${response.status}`);
+        }
+        const data = await response.json();
+        setAnimes(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchedAnimes();
+  }, []);
+
+  if (loading) return <p>Carregando...</p>;
+
+  return (
+    <div className=" w-full flex justify-center">
+      <div className="flex-wrap w-11/12 text-center flex justify-center text-slate-200">
+        {animes.map((anime) => (
+          <div className="flex w-2/5 h-94 m-2 bg-black">
+
+            <div className="w-2/5 h-full bg-cover bg-left sm:bg-contain bg-no-repeat" style={{ backgroundImage: `url(${anime.imageUrl})` }}></div>
+
+            <div className="flex flex-col p-4 gap-2 items-center text-center w-3/5 h-full">
+              <h2 className="text-xl">{anime.name}</h2>
+              <p>Ano de lançamento: {anime.releaseYear}</p>
+              <section className="flex justify-evenly">
+                <p>
+                  Episódios: {anime.episodesCount} / {anime.status}
+                </p>
+              </section>
+              <section>
+                <h2 className="text-xl">Resumo</h2>
+                <p className="text-left text-xs">{anime.summary}</p>
+              </section>
+              <section>
+                <h3 className="mb-2">Plataformas de streaming</h3>
+                <ul className="flex justify-evenly flex-row gap-2">
+                  {anime.streamingPlatforms.map((platform) => (
+                    <li className='text-sm' key={platform}>{platform}</li>
+                  ))}
+                </ul>
+              </section>
+            </div>
+
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AnimeList;
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,128 +1,14 @@
 import Header from './components/header';
+import AnimeList from './components/AnimeList';
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center min-h-screen gap-4 font-[family-name:var(--font-geist-sans)] h-screen">
+      
       <Header />
 
-      <div className="flex-wrap w-11/12 text-center flex justify-center text-slate-200">
-        <div className="flex w-2/5 h-94 m-2 bg-black">
-          <div className="w-3/6 h-full bg-white"></div>
-          <div className="flex flex-col p-4 gap-2 text-center w-3/6 h-full">
-            <h2>One Piece</h2>
-            <p>Lançamento: 20/10/1999</p>
-            <section className="flex justify-evenly">
-              <p>Eps.: 1122 / Andamento</p>
-            </section>
-            <section>
-              <h2 className="text-xl">Resumo</h2>
-              <p className="text-left text-xs">
-                Luffy, um jovem cujo corpo ganhou as propriedades de borracha
-                após ter comido uma fruta do diabo acidentalmente. Com sua
-                tripulação, os Piratas do Chapéu de Palha, Luffy explora a Grand
-                Line em busca do tesouro mais procurado do mundo, o "One Piece",
-                a fim de se tornar o próximo Rei dos Piratas.
-              </p>
-            </section>
-            <section>
-              <h3 className="mb-2">Streaming</h3>
-              <ul className="flex justify-evenly">
-                <li>Crunchyroll</li>
-                <li>Netflix</li>
-                <li>Max</li>
-              </ul>
-            </section>
-          </div>
-        </div>
-        <div className="flex w-2/5 h-94 m-2 bg-black">
-          <div className="w-3/6 h-full bg-white"></div>
-          <div className="flex flex-col p-4 gap-2 text-center w-3/6 h-full">
-            <h2>One Piece</h2>
-            <p>Lançamento: 20/10/1999</p>
-            <section className="flex justify-evenly">
-              <p>Temp.: 21</p>
-              <p>Eps.: 1122</p>
-            </section>
-            <section>
-              <h2 className="text-xl">Resumo</h2>
-              <p className="text-left text-xs">
-                Luffy, um jovem cujo corpo ganhou as propriedades de borracha
-                após ter comido uma fruta do diabo acidentalmente. Com sua
-                tripulação, os Piratas do Chapéu de Palha, Luffy explora a Grand
-                Line em busca do tesouro mais procurado do mundo, o "One Piece",
-                a fim de se tornar o próximo Rei dos Piratas.
-              </p>
-            </section>
-            <section>
-              <h3 className="mb-2">Streaming</h3>
-              <ul className="flex justify-evenly">
-                <li>Crunchyroll</li>
-                <li>Netflix</li>
-                <li>Max</li>
-              </ul>
-            </section>
-          </div>
-        </div>
-        <div className="flex w-2/5 h-94 m-2 bg-black">
-          <div className="w-3/6 h-full bg-white"></div>
-          <div className="flex flex-col p-4 gap-2 text-center w-3/6 h-full">
-            <h2>One Piece</h2>
-            <p>Lançamento: 20/10/1999</p>
-            <section className="flex justify-evenly">
-              <p>Temp.: 21</p>
-              <p>Eps.: 1122</p>
-            </section>
-            <section>
-              <h2 className="text-xl">Resumo</h2>
-              <p className="text-left text-xs">
-                Luffy, um jovem cujo corpo ganhou as propriedades de borracha
-                após ter comido uma fruta do diabo acidentalmente. Com sua
-                tripulação, os Piratas do Chapéu de Palha, Luffy explora a Grand
-                Line em busca do tesouro mais procurado do mundo, o "One Piece",
-                a fim de se tornar o próximo Rei dos Piratas.
-              </p>
-            </section>
-            <section>
-              <h3 className="mb-2">Streaming</h3>
-              <ul className="flex justify-evenly">
-                <li>Crunchyroll</li>
-                <li>Netflix</li>
-                <li>Max</li>
-              </ul>
-            </section>
-          </div>
-        </div>
-        <div className="flex w-2/5 h-94 m-2 bg-black">
-          <div className="w-3/6 h-full bg-white"></div>
-          <div className="flex flex-col p-4 gap-2 text-center w-3/6 h-full">
-            <h2>One Piece</h2>
-            <p>Lançamento: 20/10/1999</p>
-            <section className="flex justify-evenly">
-              <p>Temp.: 21</p>
-              <p>Eps.: 1122</p>
-            </section>
-            <section>
-              <h2 className="text-xl">Resumo</h2>
-              <p className="text-left text-xs">
-                Luffy, um jovem cujo corpo ganhou as propriedades de borracha
-                após ter comido uma fruta do diabo acidentalmente. Com sua
-                tripulação, os Piratas do Chapéu de Palha, Luffy explora a Grand
-                Line em busca do tesouro mais procurado do mundo, o "One Piece",
-                a fim de se tornar o próximo Rei dos Piratas.
-              </p>
-            </section>
-            <section>
-              <h3 className="mb-2">Streaming</h3>
-              <ul className="flex justify-evenly">
-                <li>Crunchyroll</li>
-                <li>Netflix</li>
-                <li>Max</li>
-              </ul>
-            </section>
-          </div>
-        </div>
-        
-      </div>
+      <AnimeList />
+
     </div>
   );
 }
